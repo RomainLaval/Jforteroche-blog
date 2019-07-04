89275277f38ae2fbc1dd50eb363d1cb6be36fea6
RomainLaval <laval.romain@gmail.com>
2019-07-04 17:05:13 +0200

    commit author fix

--- view/frontend/templateFrontend.php
+++ view/frontend/templateFrontend.php
@@ -6,33 +6,31 @@
         <link href="public/css/style.css" rel="stylesheet" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="https://use.fontawesome.com/4aa4a8c4ae.js"></script>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     </head>
 
     <body>
-    <?php
-	include('view/frontend/header.php');
-?>
-
-        <h1 id="titreLivre">Billet simple pour l'Alaska</h1>
-
-    <section class="row middle-content">
-    	<article class="col-md-8">
-                <?= $content ?>
-        </article>
-        <aside class="col-md-4" id="auteur">
-          <?php
-        include('view/frontend/author.php');
+      <?php
+	       include('view/frontend/header.php');
       ?>
-        </aside>
-    </section>
-    </div>
+
+      <h1 id="titreLivre">Billet simple pour l'Alaska</h1>
+        <section class="row middle-content">
+    	     <article class="col-md-8">
+             <?= $content ?>
+           </article>
+           <aside class="col-md-4" id="auteur">
+             <?php
+              include('view/frontend/author.php');
+              ?>
+            </aside>
+        </section>
 
     <?php
-	include('view/frontend/footer.php');
-?>
+	   include('view/frontend/footer.php');
+     ?>
 </body>
 </html>
