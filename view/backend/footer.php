9e7a7c1f2324a9c88666de27c0e3a4b14566cb65
RomainLaval <laval.romain@gmail.com>
2019-07-17 16:28:02 +0200

    ajout element admin header footer et template backend

---
+++ view/backend/footer.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+    <a href="#"> Romain Laval</a> - Projet 4 Blog écrivain
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
---
+++ view/backend/header.php
@@ -0,0 +1,40 @@
+<header>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="index.php">ADMIN</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Chapitres</a>
+                            <div class="dropdown-menu">
+                              <ul class="nav">
+                                <li><a class="nav-link" href="#">Créer Chapitre</a></li>
+                                <li><a class="nav-link" href="#">Listing Chapitres</a></li>
+                              </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#auteur">Commentaires</a>
+                            <div class="dropdown-menu">
+                              <ul class="nav">
+                                <li><a class="nav-link" href="#">Modérations commentaires</a></li>
+                                <li><a class="nav-link" href="#">Listing Commentaires</a></li>
+                              </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?action=Contact">CRM</a>
+                            <ul class="nav">
+                              <li><a class="nav-link" href="#">Liste des membres</a></li>
+                              <li><a class="nav-link" href="#">Ajouter membre</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                <a class="nav-link" href="#">Déconnexion</a>
+                </div>
+            </nav>
+    </header>
