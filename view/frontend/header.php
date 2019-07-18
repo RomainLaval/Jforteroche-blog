9a764a64b527b78ea3e9020eb341c278290e9b32
RomainLaval <laval.romain@gmail.com>
2019-07-18 09:42:38 +0200

    fix bug oubli fermeture de commentaires

--- view/frontend/header.php
+++ view/frontend/header.php
@@ -18,26 +18,3 @@
     </div>
   </nav>
     </header>
-
-
-    <!--  <div class="container">
-          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-              <a class="navbar-brand" href="index.php"></a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarText">
-                  <ul class="navbar-nav mr-auto">
-                      <li class="nav-item active">
-                          <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
-                      </li>
-                      <li class="nav-item">
-                          <a class="nav-link" href="#">Chapitres</a>
-                      </li>
-                      <li class="nav-item">
-                          <a class="nav-link" href="#auteur">L'Auteur</a>
-                      </li>
-                      <li class="nav-item">
-                          <a class="nav-link" href="index.php?action=Contact">Nous Contacter</a>
-                      </li>
-                  </ul>
